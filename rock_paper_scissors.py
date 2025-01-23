54250c129d9e6ee3a0dc19275df5e0f0fa22413b
gabxetc <gvchitamu@gmail.com>
2025-01-23 13:40:59 +0200

    Working quit optio. Working game 8.2/10

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -2,6 +2,7 @@ import random
 
 yes = "Y"
 no = "N"
+exit_game = "E"
 
 def intro():
     print("\nWelcome to a game of Rock, Paper or Scissors!")
@@ -10,6 +11,7 @@ def intro():
     print(f"\nHi {name}!\nThe rules are as follows.")
     print("1. Enter R for rock, P for paper and S for scissors")
     print("2. There are 3 rounds. Best out of 3 wins!")
+    print("3. Enter E to exit the game")
 
     print("Let the games begin!\n")
     game()
@@ -29,7 +31,7 @@ def game():
         computer_options = [rock, paper, scissors]
         computer_choice = random.choice(computer_options) 
 
-        if player_choice == rock or player_choice == paper or player_choice == scissors:
+        if player_choice == rock or player_choice == paper or player_choice == scissors or player_choice == exit_game:
             pass
             # Player loses
             if player_choice == rock and computer_choice == paper:
@@ -77,31 +79,49 @@ def game():
                 print(f"Your choice: {player_choice}")
                 print(f"Computer choice: {computer_choice}")
                 print("You both picked the same option!\n")
+            
+            # Exit game
+            elif player_choice == exit_game:
+                quitting = input("\nYou are about to exit the game. Are you sure? Y or N: ").upper()
+                if quitting == yes:
+                    quit
+                    break
+                    quit_game()
+                else:
+                    print("The game will restart now...\n")
+                    intro()
+
         else:
             print("You have put an invalid answer. The game will restart...\n")
             game()
 
-    if player_points > computer_points:
-        print("You win the game!")
+    if rounds == 3 and player_points > computer_points:
+        print("You win the game!\n")
         play_again()
-    elif computer_points > player_points:
-        print("You lost the game!")
+    elif rounds == 3 and computer_points > player_points:
+        print("You lost the game!\n")
         play_again()
-    elif player_points == computer_points:
-        print("It's a draw!")
+    elif rounds == 3 and player_points == computer_points:
+        print("It's a draw!\n")
         play_again()
+    elif rounds < 3 and player_choice == exit_game:
+        quit_game()
     else:
         print("The game has ended but no one has won...")
         play_again()
 
 def play_again():
-    question = input("Would you like to play again? Y or N: \n").upper()
+    question = input("Would you like to play again? Y or N: ").upper()
     if question == yes:
         intro()
     elif question == no:
         quit
     else:
         play_again()
+
+def quit_game():
+    print("\nGoodbye!")
+    quit
 
 if __name__ == "__main__":
     intro()
